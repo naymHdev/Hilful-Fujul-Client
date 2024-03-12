7d8672b38d9b644f26070d58fb70a3452222e6da
MacBook Pro <macbookpro@MacBooks-MBP.Dlink>
2024-03-12 23:02:13 +0600

    create a custom hook for onations card

--- src/Pages/Causes/Causes.jsx
+++ src/Pages/Causes/Causes.jsx
@@ -1,6 +1,10 @@
+import IsDonations from "../../Hooks/IsDonations";
 import SectionDetails from "../../Hooks/SectionDetails";
+import useDonations from "../../Hooks/useDonations";
 
 const Causes = () => {
+  const [isDonations] = useDonations();
+
   return (
     <>
       <SectionDetails
@@ -8,6 +12,17 @@ const Causes = () => {
         sectionTitle="Be the reason of someone smiles"
         sectionDetails="We exist for non-profits, social enterprises, community groups, activists,lorem politicians and individual citizens that are making."
       />
+      <section className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 my-20">
+        {isDonations?.map((card) => (
+          <div key={card._id}>
+            <IsDonations
+              image={card?.image}
+              title={card?.title}
+              _id={card?._id}
+            />
+          </div>
+        ))}
+      </section>
     </>
   );
 };
--- src/Pages/Donations/DonationCard.jsx
+++ src/Pages/Donations/DonationCard.jsx
@@ -1,49 +1,14 @@
 /* eslint-disable react/prop-types */
-import { Progress } from "antd";
-import { Link } from "react-router-dom";
+
+import IsDonations from "../../Hooks/IsDonations";
 
 const DonationCard = ({ casus }) => {
   const { image, title, _id } = casus || {};
 
   return (
     <>
-      <section className=" bg-gray-50 rounded-b-md shadow-md hover:shadow-2xl hover:rounded-none shadow-gray-400">
-        <div>
-          <img className=" w-full h-96" src={image} alt="" />
-        </div>
-        <div className=" w-10/12 mx-auto py-5">
-          <div>
-            <h2 className="py-3 text-xl font-bold">{title}</h2>
-
-            <div className="mt-5 mb-5">
-              <div>
-                <Progress percent={50} status="active" />
-              </div>
-              <div className=" flex items-center justify-between text-xs">
-                <div className=" flex items-center">
-                  <p>Raised:</p>
-                  <p>$7,000.00</p>
-                </div>
-                <div className=" flex items-center">
-                  <p>Goal:</p>
-                  <p>$10,000.00</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className=" flex items-center justify-center font-medium w-full">
-            <Link to={`/donationDetails/${_id}`}>
-              <div className="py-3 px-6 border-t text-center w-full">
-                Learn More
-              </div>
-            </Link>
-            <Link to="/donate">
-              <div className="text-center bg-[#31C8CC] text-white w-full py-3 px-6">
-                Donate Now
-              </div>
-            </Link>
-          </div>
-        </div>
+      <section>
+        <IsDonations image={image} _id={_id} title={title} />
       </section>
     </>
   );
