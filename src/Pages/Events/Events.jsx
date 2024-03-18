dd9591936824fe56402e769fe5223f62cae1dcce
MacBook Pro <naym100m@gmail.com>
2024-03-18 12:01:20 +0600

    updated a events page || banner section

--- src/Pages/Events/Events.jsx
+++ src/Pages/Events/Events.jsx
@@ -3,6 +3,7 @@ import EventCard from "./EventCard";
 import Button from "../../Components/Button";
 import IsBanner from "../../Hooks/IsBanner";
 import eventImage from "../../assets/eventRoute.jpg";
+import { Breadcrumb } from "antd";
 
 const Events = () => {
   const [isEvents] = useEvents();
@@ -10,12 +11,31 @@ const Events = () => {
 
   return (
     <>
+      <section className=" mt-5">
+        <Breadcrumb
+          className="text-xl font-semibold"
+          separator=">"
+          items={[
+            {
+              href: "/",
+              title: "Home",
+            },
+            {
+              title: "Events",
+            },
+          ]}
+        />
+      </section>
       <IsBanner
         headerTitle="Stride for a Cause: Mapping Our Charity Walk Route"
         description="Discover the Path of Purpose: Navigate through our charity event route page to explore the journey of giving back and making a difference, step by step."
         image={eventImage}
       />
-      <section className="py-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <hr />
+      <h3 className=" text-2xl md:text-4xl font-semibold  md:mt-12 mt-5">
+        Browse nonprofit fundraisers Events
+      </h3>
+      <section className="py-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {isEvents?.map((item) => (
           <EventCard key={item._id} event={item} />
         ))}
