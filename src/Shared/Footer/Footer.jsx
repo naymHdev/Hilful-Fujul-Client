0e28174e14505212c845311aabf4ab66573d6ef8
MacBook Pro <naym100m@gmail.com>
2024-03-18 11:11:21 +0600

    updated and modified || Footer section ||

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -6,15 +6,15 @@ import { FaFacebookF, FaYoutube, FaTwitter, FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="bg-[#302D51]">
+    <div className=" text-slate-800 border-t-2">
       <footer className="footer rounded-sm px-8 py-20 grid grid-cols-2 md:grid-cols-4">
         <div>
           <div>
             <Link to="/">
-              <h3 className=" text-3xl font-bold text-white">Hilf al-Fudul</h3>
+              <h3 className=" text-3xl font-bold text-green-700">Hilf al-Fudul</h3>
             </Link>
           </div>
-          <p className=" text-sm mt-3 text-slate-50">
+          <p className=" text-sm mt-3 text-slate-800">
             It fits perfectly for your human charity! It evokes nobility,
             collective action, and standing up for the weak, all valuable traits
             for a charitable organization.
@@ -41,45 +41,45 @@ const Footer = () => {
           </div>
         </div>
         {/* Urgent causes */}
-        <div className="text-white hover:text-green-400">
-          <header className=" text-slate-200 text-2xl font-medium">
+        <div className="text-slate-800 hover:text-green-400">
+          <header className=" text-green-700 text-2xl font-medium">
             Urgent causes
           </header>
         </div>
         {/* Quick links */}
-        <div className="text-white">
-          <header className=" text-slate-200 text-2xl font-medium">
+        <div className="text-slate-800">
+          <header className=" text-green-700 text-2xl font-medium">
             Quick links
           </header>
           <section>
             <Link to="/about">
-              <div className="mt-3 flex hover:text-green-400 items-center gap-1 text-xl font-medium">
-                <MdKeyboardArrowRight className=" text-2xl" /> About
+              <div className="mt-3 flex hover:text-green-400 items-center gap-1 font-medium">
+                <MdKeyboardArrowRight className=" " /> About
               </div>
             </Link>
             <div className=" border-b-2 mt-3 mb-2 border-slate-400"></div>
             <Link to="/blogs">
-              <div className=" flex hover:text-green-400 items-center gap-1 text-xl font-medium">
-                <MdKeyboardArrowRight className=" text-2xl" /> Blogs
+              <div className=" flex hover:text-green-400 items-center gap-1 font-medium">
+                <MdKeyboardArrowRight className=" " /> Blogs
               </div>
             </Link>
             <div className=" border-b-2 mt-3 mb-2 border-slate-400"></div>
             <Link to="/events">
-              <div className=" flex hover:text-green-400 items-center gap-1 text-xl font-medium">
-                <MdKeyboardArrowRight className=" text-2xl" /> Events
+              <div className=" flex hover:text-green-400 items-center gap-1 font-medium">
+                <MdKeyboardArrowRight className=" " /> Events
               </div>
             </Link>
             <div className=" border-b-2 mt-3 mb-2 border-slate-400"></div>
             <Link to="/donations">
-              <div className="flex hover:text-green-400 items-center gap-1 text-xl font-medium">
-                <MdKeyboardArrowRight className=" text-2xl" /> Donation
+              <div className="flex hover:text-green-400 items-center gap-1 font-medium">
+                <MdKeyboardArrowRight className=" " /> Donation
               </div>
             </Link>
           </section>
         </div>
         {/* Contact info */}
-        <div className="text-white">
-          <header className=" text-slate-200 text-2xl font-medium">
+        <div className="text-slate-800">
+          <header className=" text-green-700 text-2xl font-medium">
             Contact info
           </header>
           <section>
@@ -102,12 +102,12 @@ const Footer = () => {
           </section>
         </div>
       </footer>
-      <div className="divider divider-accent w-11/12 mx-auto"></div>
+      <hr className=" py-2" />
       <section>
-        <div className=" flex gap-2 text-center justify-center text-white pb-5">
+        <div className=" flex gap-2 text-center justify-center text-slate-800 pb-5">
           Copyright @2024 Hilf al-Fudul. Developed By
           <a
-            className=" font-bold text-green-200 underline"
+            className=" font-bold text-green-900 underline"
             target="_blank"
             rel="noreferrer"
             href="https://naymhossen1b.netlify.app"
