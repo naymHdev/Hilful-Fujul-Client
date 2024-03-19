8b04bc626f41bb71e1e7914c3ed9ebad58405210
MacBook Pro <naym100m@gmail.com>
2024-03-19 22:33:31 +0600

    donation tab updated

--- src/Pages/Donate/PaymentMethodTab.jsx
+++ src/Pages/Donate/PaymentMethodTab.jsx
@@ -1,33 +1,55 @@
-import { Tabs } from "antd";
+import { Tab } from "@headlessui/react";
+import ssl from "../../assets/ssl-thumb.png";
+import gPlay from "../../assets/G-play-image.png";
+import debitCard from "../../assets/debit-icon.png";
 
 const PaymentMethodTab = () => {
-  const onChange = (key) => {
-    console.log(key);
-  };
-
-  const items = [
-    {
-      key: "1",
-      label: "E-Money",
-      children: "Content of Tab Pane 1",
-    },
-    {
-      key: "2",
-      label: "Credit or debit ",
-      children: "Content of Tab Pane 2",
-    },
-    {
-      key: "3",
-      label: "Google Play",
-      children: "Content of Tab Pane 3",
-    },
-  ];
-
   return (
-    <div>
-      <section>
-        <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
-      </section>
+    <div className=" py-5">
+      <Tab.Group>
+        <Tab.List className="grid gap-3">
+          <Tab className="border rounded-xl hover:shadow-xl">
+            <div className=" flex items-center gap-10">
+              <div className="avatar">
+                <div className="w-24 mask mask-hexagon">
+                  <img src={ssl} />
+                </div>
+              </div>
+              <h3 className="text-xl font-bold text-slate-800">E-Payment</h3>
+            </div>
+          </Tab>
+
+          <Tab className="border rounded-xl hover:shadow-xl">
+            <div className=" flex items-center gap-10">
+              <div className="avatar">
+                <div className="w-24 mask mask-hexagon">
+                  <img src={gPlay} />
+                </div>
+              </div>
+              <h3 className="text-xl font-bold text-slate-800">Google Play</h3>
+            </div>
+          </Tab>
+          <Tab className="border rounded-xl hover:shadow-xl">
+            <div className=" flex items-center gap-10">
+              <div className="avatar">
+                <div className="w-24 mask mask-hexagon">
+                  <img src={debitCard} />
+                </div>
+              </div>
+              <h3 className="text-xl font-bold text-slate-800">
+                Credit or debit
+              </h3>
+            </div>
+          </Tab>
+        </Tab.List>
+        <section className="py-8">
+          <Tab.Panels>
+            <Tab.Panel>Content 1</Tab.Panel>
+            <Tab.Panel>Content 2</Tab.Panel>
+            <Tab.Panel>Content 3</Tab.Panel>
+          </Tab.Panels>
+        </section>
+      </Tab.Group>
     </div>
   );
 };
