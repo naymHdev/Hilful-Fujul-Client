9adfa0304466492ab63f9ee0484c42b7fdc1f01d
MacBook Pro <naym100m@gmail.com>
2024-03-23 13:48:32 +0600

    footer section modified and responsive polish

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -7,11 +7,13 @@ import { FaFacebookF, FaYoutube, FaTwitter, FaInstagram } from "react-icons/fa";
 const Footer = () => {
   return (
     <div className=" text-slate-800 border-t-2">
-      <footer className="footer rounded-sm px-8 py-10 grid grid-cols-2 md:grid-cols-4">
+      <footer className="footer rounded-sm md:px-20 px-2 md:gap-10 md:py-10 py-5 grid grid-cols-1 md:grid-cols-3">
         <div>
           <div>
             <Link to="/">
-              <h3 className=" text-3xl font-bold text-green-700">Hilf al-Fudul</h3>
+              <h3 className=" text-3xl font-bold text-green-700">
+                Hilf al-Fudul
+              </h3>
             </Link>
           </div>
           <p className=" text-sm mt-3 text-slate-800">
@@ -40,12 +42,6 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        {/* Urgent causes */}
-        <div className="text-slate-800 hover:text-green-400">
-          <header className=" text-green-700 text-2xl font-medium">
-            Urgent causes
-          </header>
-        </div>
         {/* Quick links */}
         <div className="text-slate-800">
           <header className=" text-green-700 text-2xl font-medium">
@@ -53,23 +49,23 @@ const Footer = () => {
           </header>
           <section>
             <Link to="/about">
-              <div className="mt-3 flex hover:text-green-400 items-center gap-1 font-medium">
+              <div className="mt-2 flex hover:text-green-400 items-center gap-1 font-medium">
                 <MdKeyboardArrowRight className=" " /> About
               </div>
             </Link>
-            <div className=" border-b-2 mt-3 mb-2 border-slate-400"></div>
+            <div className="mt-3 mb-2"></div>
             <Link to="/blogs">
               <div className=" flex hover:text-green-400 items-center gap-1 font-medium">
                 <MdKeyboardArrowRight className=" " /> Blogs
               </div>
             </Link>
-            <div className=" border-b-2 mt-3 mb-2 border-slate-400"></div>
+            <div className="mt-3 mb-2"></div>
             <Link to="/events">
               <div className=" flex hover:text-green-400 items-center gap-1 font-medium">
                 <MdKeyboardArrowRight className=" " /> Events
               </div>
             </Link>
-            <div className=" border-b-2 mt-3 mb-2 border-slate-400"></div>
+            <div className="mt-3 mb-2"></div>
             <Link to="/donations">
               <div className="flex hover:text-green-400 items-center gap-1 font-medium">
                 <MdKeyboardArrowRight className=" " /> Donation
@@ -82,30 +78,28 @@ const Footer = () => {
           <header className=" text-green-700 text-2xl font-medium">
             Contact info
           </header>
-          <section>
-            <div className=" mt-3 flex gap-3 hover:text-green-400 items-center font-medium">
-              <IoLocationOutline className=" text-2xl" /> 6B, Helvetica street,
+          <section className="mt-2 space-y-3">
+            <div className="flex gap-1 hover:text-green-400 items-center font-medium">
+              <IoLocationOutline className="text-xl" /> 6B, Helvetica street,
               Jordan
             </div>
-            <div className=" mt-3 flex gap-3 hover:text-green-400 items-center font-medium">
-              <IoIosCall className=" text-2xl" /> +123-456-789
+            <div className="flex gap-1 hover:text-green-400 items-center font-medium">
+              <IoIosCall className="text-xl" /> +123-456-789
             </div>
-            <div className=" border-b-2 border-slate-400 mt-6 mb-6"></div>
-
-            <div className=" mt-3 flex gap-3 hover:text-green-400 items-center font-medium">
-              <IoLocationOutline className=" text-2xl" /> 6B, Helvetica street,
+            <div className="flex gap-1 hover:text-green-400 items-center font-medium">
+              <IoLocationOutline className="text-xl" /> 6B, Helvetica street,
               Jordan
             </div>
-            <div className=" mt-3 flex gap-3 hover:text-green-400 items-center font-medium">
-              <IoIosCall className=" text-2xl" /> +123-456-789
+            <div className="flex gap-1 hover:text-green-400 items-center font-medium">
+              <IoIosCall className="text-xl" /> +123-456-789
             </div>
           </section>
         </div>
       </footer>
       <hr className=" py-2" />
       <section>
-        <div className=" flex gap-2 text-center justify-center text-slate-800 pb-5">
-          Copyright @2024 Hilf al-Fudul. Developed By
+        <div className="md:flex gap-2 text-center justify-center text-slate-800 pb-5">
+          Copyright @2024 Hilf al-Fudul. Developed By 
           <a
             className=" font-bold text-green-900 underline"
             target="_blank"
