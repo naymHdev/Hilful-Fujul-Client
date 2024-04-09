013ec0fdb695d548550ce99324430ad42650fe8d
MacBook Pro <naym100m@gmail.com>
2024-04-09 22:22:31 +0600

    modifiead a || Blog || pages

--- src/Pages/Blogs/Blogs.jsx
+++ src/Pages/Blogs/Blogs.jsx
@@ -1,41 +1,7 @@
-import { useDispatch, useSelector } from "react-redux";
-import {
-  decrement,
-  increment,
-  incrementByValue,
-} from "../../Redux/features/counter/counterSlice";
-
 const Blogs = () => {
-  const count = useSelector((state) => state.counter.count);
-  const dispatch = useDispatch();
-
-  return (
-    <div>
-      <div className=" flex items-center gap-8 h-screen">
-        <button
-          className="btn btn-primary"
-          onClick={() => dispatch(increment())}
-        >
-          Increment
-        </button>
-        <div>
-          <p>{count}</p>
-        </div>
-        <button
-          className="btn btn-warning"
-          onClick={() => dispatch(decrement())}
-        >
-          Decrement
-        </button>
-        <button
-          className="btn btn-warning"
-          onClick={() => dispatch(incrementByValue(3))}
-        >
-          Increment By Value
-        </button>
-      </div>
-    </div>
-  );
+  return <div>
+    <p>Blogs</p>
+  </div>;
 };
 
 export default Blogs;
