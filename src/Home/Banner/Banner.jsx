3e0d8b2d95891673c494f0b0d888b54a186e8eb3
MacBook Pro <naym100m@gmail.com>
2024-08-17 19:03:34 +0600

    [compleate] the header, navbar and banner section with full responsive

--- src/Home/Banner/Banner.jsx
+++ src/Home/Banner/Banner.jsx
@@ -47,36 +47,38 @@ const Banner = () => {
     <>
       <div className="slider-container">
         <Slider {...settings}>
-          <div className=" relative">
+          <div className=" relative md:h-full h-[400px]">
             <img
               className=" w-full h-full object-cover object-center"
               src={banner1}
               alt="Slider Banner"
             />
-            <div className=" text-white absolute flex flex-col space-y-10 items-center bottom-[200px] left-[300px] text-center">
-              <SiHelpscout className=" text-9xl text-red-600" />
-              <h2 className=" text-5xl font-extrabold">
-                সহমর্মিতার ছোঁয়া, মানবতার জয়
+            <div className=" text-white absolute flex flex-col space-y-4 md:space-y-6 lg:space-y-10 items-center lg:bottom-[200px] bottom-14 md:bottom-[100px] md:left-[14px] lg:left-[300px] text-center">
+              <SiHelpscout className=" lg:text-9xl text-6xl text-red-600" />
+              <h2 className=" text-2xl md:text-5xl font-extrabold">
+                সহমর্মিতার ছোঁয়া,{" "}
+                <span className=" text-green-600">মানবতার</span> জয়
               </h2>
-              <p className=" font-bold">
+              <p className="font-medium lg:font-bold">
                 আপনার ছোট্ট সহায়তায় গড়ে উঠতে পারে একজনের সুখী জীবন। দরিদ্র ও
                 অভাবীদের পাশে দাঁড়িয়ে মানবতার সেবা করুন।
               </p>
               <Button label="যোগ দিন" />
             </div>
           </div>
-          <div className=" relative">
+          <div className=" relative md:h-full h-[400px]">
             <img
               className=" w-full h-full object-cover object-center"
               src={banner2}
               alt=""
             />
-            <div className=" text-white absolute flex flex-col space-y-10 items-center bottom-[200px] left-[300px] text-center">
-              <SiHelpscout className=" text-9xl text-green-600" />
-              <h2 className=" text-5xl font-extrabold">
-                প্রকৃতির সুরক্ষায় আপনার অংশীদারিত্ব
+            <div className=" text-white absolute flex flex-col space-y-4 md:space-y-6 lg:space-y-10 items-center lg:bottom-[200px] bottom-14 md:bottom-[100px] md:left-[14px] lg:left-[300px] text-center">
+              <SiHelpscout className="lg:text-9xl text-6xl text-green-600" />
+              <h2 className="text-2xl md:text-5xl font-extrabold">
+                প্রকৃতির সুরক্ষায় আপনার{" "}
+                <span className=" text-red-600">অংশীদারিত্ব</span>
               </h2>
-              <p className=" font-bold">
+              <p className="font-medium lg:font-bold">
                 আপনার সহায়তায় পরিবেশ রক্ষা করুন। একসাথে আমরা সবুজ পৃথিবী গড়তে
                 পারি।
               </p>
