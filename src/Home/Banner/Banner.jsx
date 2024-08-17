c7c0264f38d164c85bda5d0539e97b6948f2338f
MacBook Pro <naym100m@gmail.com>
2024-08-17 12:44:31 +0600

    [created] a new banner with slider imag caruole

--- src/Home/Banner/Banner.jsx
+++ src/Home/Banner/Banner.jsx
@@ -1,16 +1,55 @@
-// import bg from "../../../src/assets/bannerBgImage.jpg"
+import Slider from "react-slick";
+import banner1 from "../../../src/assets/sliderB1.jpg";
+import banner2 from "../../../src/assets/sliderB2.jpg";
 
 const Banner = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <>
-      <div
-        className="w-full h-screen bg-cover bg-current bg-no-repeat"
-        style={{
-          backgroundImage: `url('/src/assets/bannerBgImage.jpg'), url('/fallbackBg.jpg')`,
-          backgroundColor: "#f3f4f6",
-        }}
-      >
-        <div></div>
+      <div className="slider-container">
+        <Slider {...settings}>
+          <div>
+            <img src={banner1} alt="Slider Banner" />
+          </div>
+          <div>
+            <img src={banner2} alt="" />
+          </div>
+        </Slider>
       </div>
     </>
   );
