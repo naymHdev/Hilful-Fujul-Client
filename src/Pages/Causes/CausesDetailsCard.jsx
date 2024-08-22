adec3904d2261f87c3b9f8eef3c1833927ba0b45
MacBook Pro <naym100m@gmail.com>
2024-08-22 21:49:27 +0600

    [created] a project details route with responsive"

--- src/Pages/Causes/CausesDetailsCard.jsx
+++ src/Pages/Causes/CausesDetailsCard.jsx
@@ -1,17 +1,11 @@
 /* eslint-disable react/prop-types */
 
+import { Divider } from "antd";
 import DonationSection from "./DonationSection";
 
 const CausesDetailsCard = ({ details }) => {
-  const {
-    projectName,
-    images,
-    projectDescription,
-    hadiths,
-    shortDescription,
-    projectHighlights,
-    paymentSystem,
-  } = details || {};
+  const { projectName, images, projectDescription, hadiths, shortDescription } =
+    details || {};
 
   return (
     <>
@@ -19,14 +13,38 @@ const CausesDetailsCard = ({ details }) => {
         <h1 className=" text-4xl font-semibold text-black">{projectName}</h1>
 
         <section className=" grid grid-cols-1 lg:grid-cols-9 gap-4">
-          <div className=" col-span-full lg:col-span-6">
+          <div className=" col-span-full lg:col-span-6 overflow-auto overflow-y-scroll h-screen overflow-x-hidden no-scrollbar">
             <img
               className=" mt-6 w-full h-auto rounded-3xl"
               src={images[0]?.data?.display_url}
               alt={projectName}
             />
+
+            {/* Description */}
+            <div className=" w-full border-b border-slate-800 my-10" />
+            <div className="">
+              <div
+                className="prose text-sm"
+                dangerouslySetInnerHTML={{
+                  __html: shortDescription && shortDescription,
+                }}
+              />
+            </div>
+            <Divider />
+            <div className=" font-semibold text-green-700 space-y-2">
+              <p>{hadiths?.firstHadith && hadiths?.firstHadith}</p>
+              <p>{hadiths?.secondHadith && hadiths?.secondHadith}</p>
+            </div>
+            {hadiths?.firstHadith || (hadiths?.secondHadith && <Divider />)}
+
+            <div
+              className="prose text-sm mt-4"
+              dangerouslySetInnerHTML={{
+                __html: projectDescription && projectDescription,
+              }}
+            />
           </div>
-          <div className=" col-span-full lg:col-span-3">
+          <div className=" col-span-full lg:col-span-3 mb-6">
             <DonationSection />
           </div>
         </section>
--- src/Pages/Causes/DonationSection.jsx
+++ src/Pages/Causes/DonationSection.jsx
@@ -11,37 +11,41 @@ const DonationSection = () => {
   };
 
   return (
-    <div className=" rounded-3xl shadow-lg px-6 py-5">
-      <div className="">
-        <p className=" text-xl font-semibold text-black">
-          ৳ {convertToBanglaNumber(value.toLocaleString())}
-          <span className=" px-1 text-sm text-gray-700 font-normal">
-            টাকা সংগৃহীত
-          </span>
-        </p>
+    <>
+      <div className=" mt-5 md:mt-0">
+        <div className="md:rounded-3xl md:shadow-lg md:px-6">
+          <div className="">
+            <p className=" text-xl font-semibold text-black">
+              ৳ {convertToBanglaNumber(value.toLocaleString())}
+              <span className=" px-1 text-sm text-gray-700 font-normal">
+                টাকা সংগৃহীত
+              </span>
+            </p>
+          </div>
+          <div className="">
+            <input
+              type="range"
+              min="0"
+              max={maxValue}
+              value={value}
+              className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer range-thumb"
+              style={{
+                background: `linear-gradient(to right, #10b981 0%, #10b981 ${percentage}%, #e5e7eb ${percentage}%, #e5e7eb 100%)`,
+              }}
+            />
+            <p className=" text-sm text-black">100 জন দান করেছেন</p>
+          </div>
+          <div className=" mt-4">
+            <button className="w-full bg-yellow-400 text-black font-semibold py-3 px-4 rounded-xl mb-2">
+              শেয়ার করুন
+            </button>
+            <button className="w-full bg-orange-400 text-black font-semibold py-3 px-4 rounded-xl mb-6">
+              দান করুন
+            </button>
+          </div>
+        </div>
       </div>
-      <div className="">
-        <input
-          type="range"
-          min="0"
-          max={maxValue}
-          value={value}
-          className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer range-thumb"
-          style={{
-            background: `linear-gradient(to right, #10b981 0%, #10b981 ${percentage}%, #e5e7eb ${percentage}%, #e5e7eb 100%)`,
-          }}
-        />
-        <p className=" text-sm text-black">100 জন দান করেছেন</p>
-      </div>
-      <div className=" mt-4">
-        <button className="w-full bg-yellow-400 text-black font-bold py-3 px-4 rounded-xl mb-2">
-          Share
-        </button>
-        <button className="w-full bg-orange-400 text-black font-bold py-3 px-4 rounded-xl mb-6">
-          Donate now
-        </button>
-      </div>
-    </div>
+    </>
   );
 };
 
